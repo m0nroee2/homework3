9fd8b95724f4efbd82b30d37df00a8ee9d03ca73
Арсений Бочарников <senboch@gmail.com>
2022-11-29 22:40:14 +0100

    homework

---
+++ homework3/homework3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long  int n=0,resaut=1;
+	
+	while(true)
+	{
+		cout << "Napishite programu vichesl faktoriala" << endl;
+		cin >> n;
+
+		if (n == 0)
+			cout << "ERORR dead" << endl;
+		else
+			break;
+	}
+	if (n > 0) 
+	{
+		for (int i = n; i != 0; i--)
+			resaut = resaut * i;
+		
+	}
+	else
+	{
+		for (int i = n; i !=0; i++)
+			resaut = resaut * i;
+	}
+	cout << "Your faktorial " << resaut << endl;
+
+	return 0;
+}
